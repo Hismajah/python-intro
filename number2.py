acfddb2b4ff5598ea880de7bbad155c2c82fd666
hismajah <majahenitan@gmail.com>
2024-09-26 23:15:41 +0300

    my second commit

---
+++ number2.py
@@ -0,0 +1,13 @@
+a=69
+b=89
+c=49
+f=69*89*49
+f-100
+f-100/5
+print(f)
+print ("this is my total result " + str (f))
+
+if f==100:
+    print('yes it is equal to 100')
+else:
+    print('no it is not equal to 100')
---
+++ number3.py
@@ -0,0 +1,13 @@
+
+a=31
+b=41
+c=51
+f=a+b+c
+f-100/5*3
+print(f)
+print ("here is my total result " + str (f))
+
+if f==200:
+    print('yes it is equal to 200')
+else:
+    print('no it is not equal to 200')
