42d38a85e6ac21f4468422b0caf4ee09709d49d2
hismajah <majahenitan@gmail.com>
2024-10-01 14:05:31 +0300

    i created new file and folder

---
+++ user1.py
@@ -0,0 +1,8 @@
+# write a python code that will ask for user input
+
+print("enter your surname")
+surname=input()
+print("enter your firstname")
+firstname=input()
+print("details " + surname + " " + firstname)
+
---
+++ user2.py
@@ -0,0 +1,9 @@
+# write a python user input that will get your school details
+
+print ("enter your school name")
+schoolname=input()
+print ("enter your school email")
+schoolemail=input()
+print ("enter your school address")
+schooladdress=input()
+print("details " + schoolname + " " + schoolemail + " " +  schooladdress)
